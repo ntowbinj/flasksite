4b4a61007af215d07d396803f0a128bc112b16c9
n <ntowbinj@gmail.com>
2014-06-08 01:03:18 -0500

    only '/'

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
-@app.route("/eartrainer")
 def hello(): 
     dependencies = ['AudioDetect.js', 'LoadPlugin.js', 'Plugin.js', 'Player.js'] 
     dependencies = ['js/MIDI.js/js/MIDI/' + d for d in dependencies]
