bfeacf5657e4f4a42cfc72f63055af10a09bc537
n <ntowbinj@gmail.com>
2014-06-08 01:05:37 -0500

    still not

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
+@app.route("/dumb")
 def hello(): 
     dependencies = ['AudioDetect.js', 'LoadPlugin.js', 'Plugin.js', 'Player.js'] 
     dependencies = ['js/MIDI.js/js/MIDI/' + d for d in dependencies]
