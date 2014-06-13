6bc31ed2b6d281ecf8cd0614b3a43429ad0949db
n <ntowbinj@gmail.com>
2014-06-13 16:16:47 -0500

    on onload again

--- FlaskApp/static/js/music.js
+++ FlaskApp/static/js/music.js
@@ -1,4 +1,4 @@
 function musicSetup(){
     var src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/129879035&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false";
-   //$("#soundcloud").attr('src', src);
+   $("#soundcloud").attr('src', src);
 }
