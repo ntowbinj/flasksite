c214750df6ed4e6faac7e3668fea81c286ae5617
n <ntowbinj@gmail.com>
2014-07-22 21:12:17 -0500

    no box-sizing for firefox <2.9

--- FlaskApp/static/js/visuals.js
+++ FlaskApp/static/js/visuals.js
@@ -1,15 +1,26 @@
 var visual = {
     resize: function()
     {
-        return;
-        alert('didnt return');
-        var body = document.body,
-            html = document.documentElement;
+        browserversion= (function(){
+        var ua= navigator.userAgent, tem, 
+        M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if(/trident/i.test(M[1])){
+            tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return ['IE', (tem[1] || '')];
+        }
+        if(M[1]=== 'Chrome'){
+            tem= ua.match(/\bOPR\/(\d+)/)
+            if(tem!= null) return 'Opera '+tem[1];
+        }
+        M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+        return M;
+        })();
+        if(browserversion[0] == 'Firefox' && parseInt(browserversion[1]) < 29){
+            $('nav').addClass("notBoxsized");
+            $('nav').removeClass("boxsized");
+        }
 
-        var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                html.clientHeight, html.scrollHeight, html.offsetHeight );
-        var middleHeight = document.getElementById('middle').style.height;
-        document.getElementById('middle').style.height = height + 'px';
     },
     currentPage: function() {
         var title = "#" + $.trim($("title")[0].text);
